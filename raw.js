10cfbdff345ec162b4f2618c8856a94783840459
Kimmy Lindvall <kimmy.lindvall@gmail.com>
2019-02-17 02:32:57 +0100

    Remove raw matrix mode

--- raw.js
+++ raw.js
@@ -2,12 +2,6 @@
 
 let dlxControls;
 
-function parseMatrix(str) {
-  return str.split(/\n+/).map(l => l.trim()).filter(l => l).map(l => l.split(/ +/)).map(
-    l => l.map(c => c === "0" ? false : true)
-  );
-}
-
 function parseNames(str) {
   let lines = str.split(/\n+/).map(l => l.trim()).filter(l => l);
   let [columns, columnsSecondary] = lines.shift().split(/\|/, 2).map(cs => cs.trim().split(/ +/));
@@ -25,40 +19,8 @@ function parseNames(str) {
 }
 
 window.onload = function() {
-  const inputMatrix = document.getElementById("inputMatrix");
-  const runMatrix = document.getElementById("runMatrix");
-  const outputMatrix = document.getElementById("outputMatrix");
   const inputNames = document.getElementById("inputNames");
   const outputNames = document.getElementById("outputNames");
-
-  runMatrix.focus();
-  outputMatrix.value = "";
-
-  runMatrix.onclick = function() {
-    let matrix = parseMatrix(inputMatrix.value);
-
-    let gen = dlx(
-      matrix[0].map((_, i) => i),
-      matrix.map(row => ({
-        cols: row.map((val, i) => val ? i : null).filter(x => x != null)
-      }))
-    );
-
-    let solutions = [];
-    let nodes, updates;
-    for (let u of gen) {
-      solutions = [].concat(solutions, u.solutions);
-      nodes = u.nodes;
-      updates = u.updates;
-    }
-
-    outputMatrix.value = `${solutions.length} solutions, ${nodes} nodes, ${updates} updates\n\n` +
-      solutions.map(solution =>
-        solution.map(row =>
-          matrix[0].map((_, i) => row.includes(i) ? '1' : '0').join(' ')
-        ).join('\n')
-      ).join('\n\n');
-  }
 
   outputNames.value = "";
 
